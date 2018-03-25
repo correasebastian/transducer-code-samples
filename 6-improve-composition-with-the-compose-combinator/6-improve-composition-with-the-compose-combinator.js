3d1a536ab2959f9d70faf5a29e2c94340be3b5b3
sebastian correa <scorrea1@wsgc.com>
2018-03-25 12:54:34 -0400

    convert to commonjs modules and visualize compose

--- 6-improve-composition-with-the-compose-combinator/6-improve-composition-with-the-compose-combinator.js
+++ 6-improve-composition-with-the-compose-combinator/6-improve-composition-with-the-compose-combinator.js
@@ -1,13 +1,16 @@
-import {filter, map, evenOnly, doubleTheNumber} from "../utils";
+"use strict";
 
-const doubleMap = map(doubleTheNumber);
-const isEvenFilter = filter(evenOnly);
-const isNot2Filter = filter(val => val !== 2);
-const pushReducer = (accumulation, value) => {
+var _utils = require("../utils");
+
+var doubleMap = ( _utils.map)(_utils.doubleTheNumber);
+var isEvenFilter = ( _utils.filter)(_utils.evenOnly);
+var isNot2Filter = ( _utils.filter)(function (val) {
+  return val !== 2;
+});
+var pushReducer = function pushReducer(accumulation, value) {
   accumulation.push(value);
   return accumulation;
 };
-
 [1, 2, 3, 4].reduce(isNot2Filter(isEvenFilter(doubleMap(pushReducer))), []);
 
 // compose(f,g)(x) === f(g(x));
@@ -15,11 +18,88 @@ const pushReducer = (accumulation, value) => {
 // compose(isNot2Filter, isEvenFilter, doubleMap)(pushReducer) ===
 // isNot2Filter(isEvenFilter(doubleMap(pushReducer)));
 
-const compose = (...functions) =>
-  functions.reduce((accumulation, fn) =>
-    (...args) => accumulation(fn(...args)), x => x);
+function double(x) {
+    return 2*x
+}
 
-[1, 2, 3, 4].reduce(
+function triple(x) {
+  return 3*x
+}
+
+
+function restOne(x) {
+  return x-1
+}
+
+const fnName = /function\s(.*){/g
+
+const getNameFromfn = (fn) => {
+  // const arr= null/*  = fnName.exec(fn.toString()); */
+  // // console.log(arr);
+  return fn.nickName
+    ? fn.nickName
+
+    : fn.name
+}
+
+const compose = (...functions) =>
+  functions.reduce(function mainReducer(accumulation, fn, i, initialArray) {
+    const accName = getNameFromfn(accumulation)
+    const fnName = getNameFromfn(fn)
+
+    console.log('accumulation', accName );
+    console.log('fn ' + fnName  );
+
+    const rtnFnName = `fnRtn${i}`
+    const rtnFn = `
+        function ${rtnFnName}(...args){
+          // thanks to closure
+          return ${accName}(${fnName}(...args))
+        }
+    `
+
+    console.log(rtnFn);
+
+    let called = 0
+
+       function shubby (arg) { 
+        //  if(i=== initialArray.length -1){
+          const my = shubby
+          const idx = i 
+          called +=1
+          if(called === 1){
+
+            // console.log(`*******************  ${i}: ${initialArray.length - i} : ${called}   ******************* `  );
+            console.log(`*******************  ${rtnFnName} with ${arg}   ******************* `  );
+            // console.log(rtnFnName + ' with ', arg );
+            const mostInnerResult = fn(arg);
+            console.log(`${fnName}(${arg}) : ${mostInnerResult} ` );
+            if(idx===0){
+              console.log(`*******************  ${accName} with ${mostInnerResult}   ******************* `  );
+              // console.log(`${accName}(${mostInnerResult}) : ${accumulation(mostInnerResult)} ` );
+            }
+          }
+        //  }
+        
+        return accumulation(fn(arg))
+      }
+
+      shubby.nickName= rtnFnName;
+      // @ts-ignore
+      shubby.description = rtnFn;
+
+      return shubby
+    }
+    ,
+    function identity (x) {
+      return x
+    }
+  );
+
+
+  const w= compose(double, restOne, triple)(5)
+  console.log(w);
+/* [1, 2, 3, 4].reduce(
   compose(isNot2Filter, isEvenFilter, doubleMap)(pushReducer),
   [],
-); /*?*/
+); */
--- utils.js
+++ utils.js
@@ -1,81 +1,126 @@
-import {isPlainObject, entries} from 'lodash';
+'use strict';
 
-export const timeIt = (label, fn) => {
-  console.time(label);
-  fn();
-  console.timeEnd(label);
+exports.__esModule = true;
+exports.seq = exports.transduce = exports.into = exports.objectReducer = exports.arrayReducer = exports.scream = exports.shout = exports.toUpper = exports.pushReducer = exports.doubleAndEven = exports.doubleTheNumber = exports.evenOnly = exports.filter = exports.map = exports.compose = exports.arrayofRandoms = exports.timeIt = undefined;
+
+var _lodash = require('lodash');
+
+var timeIt = exports.timeIt = function timeIt(label, fn) {
+    console.time(label);
+    fn();
+    console.timeEnd(label);
 };
 
-export const arrayofRandoms = randomCeil => length =>
-  Array.from({length: length}, (v, i) =>
-    Math.floor(Math.random() * randomCeil));
-
-export const compose = (...functions) =>
-    functions.reduce((accumulation, fn) =>
-        (...args) => accumulation(fn(...args)), x => x);
-
-export const map = xf => reducer => {
-    return (accumulation, value) => {
-        return reducer(accumulation, xf(value));
+var arrayofRandoms = exports.arrayofRandoms = function arrayofRandoms(randomCeil) {
+    return function (length) {
+        return Array.from({ length: length }, function (v, i) {
+            return Math.floor(Math.random() * randomCeil);
+        });
     };
 };
 
-export const filter = predicate => reducer => {
-    return (accumulation, value) => {
-        if (predicate(value)) return reducer(accumulation, value);
-        return accumulation;
+var compose = exports.compose = function compose() {
+    for (var _len = arguments.length, functions = Array(_len), _key = 0; _key < _len; _key++) {
+        functions[_key] = arguments[_key];
+    }
+
+    return functions.reduce(function (accumulation, fn) {
+        return function () {
+            return accumulation(fn.apply(undefined, arguments));
+        };
+    }, function (x) {
+        return x;
+    });
+};
+
+var map = exports.map = function map(xf) {
+    return function (reducer) {
+        return function (accumulation, value) {
+            return reducer(accumulation, xf(value));
+        };
     };
 };
 
-export const evenOnly = number => number % 2 === 0;
-export const doubleTheNumber = number => number * 2;
-export const doubleAndEven = compose(map(doubleTheNumber), filter(evenOnly));
-export const pushReducer = (accumulation, value) => {
+var filter = exports.filter = function filter(predicate) {
+    return function (reducer) {
+        return function (accumulation, value) {
+            if (predicate(value)) return reducer(accumulation, value);
+            return accumulation;
+        };
+    };
+};
+
+var evenOnly = exports.evenOnly = function evenOnly(number) {
+    return number % 2 === 0;
+};
+var doubleTheNumber = exports.doubleTheNumber = function doubleTheNumber(number) {
+    return number * 2;
+};
+var doubleAndEven = exports.doubleAndEven = compose(map(doubleTheNumber), filter(evenOnly));
+var pushReducer = exports.pushReducer = function pushReducer(accumulation, value) {
     accumulation.push(value);
     return accumulation;
 };
-export const toUpper = str => str.toUpperCase();
-export const shout = str => `${str}!!`;
-export const scream = str => toUpper(shout(str));
+var toUpper = exports.toUpper = function toUpper(str) {
+    return str.toUpperCase();
+};
+var shout = exports.shout = function shout(str) {
+    return str + '!!';
+};
+var scream = exports.scream = function scream(str) {
+    return toUpper(shout(str));
+};
 
-export const arrayReducer = (array, value) => {
+var arrayReducer = exports.arrayReducer = function arrayReducer(array, value) {
     array.push(value);
     return array;
 };
 
 // and our object reducer will perform a shallow merge with object.assign.
-export const objectReducer = (obj, value) => Object.assign(obj, value);
+var objectReducer = exports.objectReducer = function objectReducer(obj, value) {
+    return Object.assign(obj, value);
+};
 
-export const into = (to, xf, collection) => {
-    if (Array.isArray(to)) return transduce(xf, arrayReducer, to, collection);
-    else if (isPlainObject(to)) return transduce(xf, objectReducer, to, collection);
+var into = exports.into = function into(to, xf, collection) {
+    if (Array.isArray(to)) return transduce(xf, arrayReducer, to, collection);else if ((0, _lodash.isPlainObject)(to)) return transduce(xf, objectReducer, to, collection);
     throw new Error('into only supports arrays and objects as `to`');
 };
 
-export const transduce = (xf /** could be composed **/, reducer, seed, _collection) => {
+var transduce = exports.transduce = function transduce(xf /** could be composed **/, reducer, seed, _collection) {
 
     // apply our reducer transform
-    const transformedReducer = xf(reducer);
+    var transformedReducer = xf(reducer);
     // for every value, send the current value and the new total in, function returns a new total
-    let accumulation = seed;
+    var accumulation = seed;
 
-    const collection = isPlainObject(_collection) ? entries(_collection) : _collection;
+    var collection = (0, _lodash.isPlainObject)(_collection) ? (0, _lodash.entries)(_collection) : _collection;
 
-    for (let value of collection) {
+    for (var _iterator = collection, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
+        var _ref;
+
+        if (_isArray) {
+            if (_i >= _iterator.length) break;
+            _ref = _iterator[_i++];
+        } else {
+            _i = _iterator.next();
+            if (_i.done) break;
+            _ref = _i.value;
+        }
+
+        var value = _ref;
+
         accumulation = transformedReducer(accumulation, value); /*?*/
     }
 
     return accumulation;
 };
 
-export const seq = (xf, collection) => {
+var seq = exports.seq = function seq(xf, collection) {
     if (Array.isArray(collection)) return transduce(xf, arrayReducer, [], collection);
     // and for an object it will be an empty object
-    else if (isPlainObject(collection)) return transduce(xf, objectReducer, {}, collection);
-    else if (collection['@@transducer/step']) {
-        const init = collection['@@transducer/init'] ? collection['@@transducer/init']() : collection.constructor();
-        return transduce(xf, collection['@@transducer/step'], init, collection);
-
-    }
+    else if ((0, _lodash.isPlainObject)(collection)) return transduce(xf, objectReducer, {}, collection);else if (collection['@@transducer/step']) {
+            var init = collection['@@transducer/init'] ? collection['@@transducer/init']() : collection.constructor();
+            return transduce(xf, collection['@@transducer/step'], init, collection);
+        }
     throw new Error('unsupported data type');
 };
